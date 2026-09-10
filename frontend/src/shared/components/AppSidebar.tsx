e8a3f733472e9effdee99b9a20255cefa6c9245d
tetsuo-kouyama <kouyamatetuo1129@gmail.com>
2026-09-11 04:17:47 +0900

    feat: サイドバーのレイアウトを作成

---
+++ frontend/src/shared/components/AppSidebar.tsx
@@ -0,0 +1,13 @@
+import { Sidebar, SidebarHeader } from "@/components/ui/sidebar"
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <span className="px-2 py-2 text-xl font-bold">
+          Timeventure
+        </span>
+      </SidebarHeader>
+    </Sidebar>
+  )
+}
