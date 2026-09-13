0c2ef75dc91957f89a2055ac727d5a34b7df8294
tetsuo-kouyama <kouyamatetuo1129@gmail.com>
2026-09-13 18:02:39 +0900

    feat: タイマー完了時の処理を実装

--- frontend/src/features/timer/components/TimerPanel.tsx
+++ frontend/src/features/timer/components/TimerPanel.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { SECONDS_PER_MINUTE } from "../constants/timer";
-import type { TimerSetting } from "../types/timer";
+import type { TimerMode, TimerSetting } from "../types/timer";
 import { TimerDisplay } from "./TimerDisplay";
 import { Button } from "@/components/ui/button";
 
@@ -11,7 +11,7 @@ type Props = {
 export function TimerPanel({ setting }: Props) {
 
   // タイマーのモード（"focus": 集中 / "break": 休憩）
-  // const [mode, setMode] = useState<TimerMode>("focus")
+  const [mode, setMode] = useState<TimerMode>("focus")
 
   // タイマーの稼働状態（true: 実行中 / false: 停止中）
   const [isRunning, setIsRunning] = useState(false)
@@ -26,17 +26,42 @@ export function TimerPanel({ setting }: Props) {
     setIsRunning(true)  // 「実行中」に変更
   }
 
+  // タイマーを止める処理
+  const resetTimer = () => {
+    setMode("focus")
+    setIsRunning(false)
+    setRemainingSeconds(
+      setting.focus_minutes * SECONDS_PER_MINUTE
+    )
+  }
+
   // タイマー実行中は1秒ごとに残り時間を減らす
   useEffect(() => {
     if (!isRunning) return
 
     const timer = setInterval(() => {
       setRemainingSeconds((prev) => {
-        if (prev <= 0) {
-          return 0
+        if (prev > 1) {
+          return prev - 1
         }
 
-        return prev - 1
+        if (mode === "focus") {
+          if (setting.break_minutes === 0) {
+            setMode("focus")
+            setIsRunning(false)
+
+            return setting.focus_minutes * SECONDS_PER_MINUTE
+          }
+
+          setMode("break")
+
+          return setting.break_minutes * SECONDS_PER_MINUTE
+        }
+
+        setMode("focus")
+        setIsRunning(false)
+
+        return setting.focus_minutes + SECONDS_PER_MINUTE
       })
     }, 1000)
 
@@ -44,11 +69,16 @@ export function TimerPanel({ setting }: Props) {
     return () => {
       clearInterval(timer)
     }
-  }, [isRunning])
+  }, [
+    isRunning,
+    mode,
+    setting.focus_minutes,
+    setting.break_minutes,
+  ])
 
   return (
     <div>
-      <TimerDisplay remainingSeconds={remainingSeconds} />
+      <TimerDisplay remainingSeconds={remainingSeconds} mode={mode} />
 
       {/* 「停止中」は「スタート」を表示 */}
       {!isRunning && (
@@ -57,10 +87,17 @@ export function TimerPanel({ setting }: Props) {
         </Button>
       )}
 
-      {/* 「実行中」は「中断する」を表示 */}
-      {isRunning && (
+      {/* 「focus中」は「中断する」を表示  */}
+      {isRunning && mode === "focus" && (
         <Button type="button" variant="destructive">
           中断する
+        </Button>
+      )}
+
+      {/* 「break中」は「終了する」を表示」 */}
+      {isRunning && mode === "break" && (
+        <Button type="button" onClick={resetTimer}>
+          終了する
         </Button>
       )}
     </div>
