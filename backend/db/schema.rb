36f3418a6913641c06eb299746155f81deae22ad
tetsuo-kouyama <kouyamatetuo1129@gmail.com>
2026-09-14 12:22:17 +0900

    feat: Areasテーブルの作成

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_14_015422) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_14_022513) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "areas", force: :cascade do |t|
+    t.integer "area_type", null: false
+    t.integer "battle_weight", null: false
+    t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.bigint "prerequisite_area_id"
+    t.integer "treasure_weight", null: false
+    t.datetime "updated_at", null: false
+    t.index ["prerequisite_area_id"], name: "index_areas_on_prerequisite_area_id"
+  end
 
   create_table "enemies", force: :cascade do |t|
     t.integer "base_attack", null: false
@@ -54,6 +65,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_14_015422) do
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
+  add_foreign_key "areas", "areas", column: "prerequisite_area_id"
   add_foreign_key "sessions", "users"
   add_foreign_key "timer_settings", "users"
 end
