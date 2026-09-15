ae22eae0c35d6e8d85392949448fcfb14db45b6a
tetsuo-kouyama <kouyamatetuo1129@gmail.com>
2026-09-15 23:56:40 +0900

    fix: login メソッドの引数を修正

--- backend/spec/support/request_helpers.rb
+++ backend/spec/support/request_helpers.rb
@@ -1,10 +1,8 @@
 module RequestHelpers
   def login(user, password: "password")
     post api_v1_session_path, params: {
-      session: {
-        email_address: user.email_address,
-        password: user.password
+      email_address: user.email_address,
+      password: password
       }
-    }
   end
 end
